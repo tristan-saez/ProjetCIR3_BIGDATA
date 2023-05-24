5884aa1a6090f9f3063d13157a051ec8bf58731c
Vincent <lebrennv@gmail.com>
2023-05-24 11:49:00 +0200

    Vrai regression lineaire mais sans commentaire

--- regression_lineaire_mois.R
+++ regression_lineaire_mois.R
@@ -1,6 +1,4 @@
-setwd("C:/Users/Vincent/Desktop/Projet-Big-Data/Test")
 
-getwd()
 library("car")
 library("readxl")
 
@@ -20,9 +18,11 @@ mois_tmp <- as.data.frame(mois_tmp)
 
 #Mois
 mois_accident <- data.frame(Var1 = factor(mois_tmp$Var1), Freq = mois_tmp$Freq)
+mois_accident$CumFreq <- cumsum(mois_accident$Freq)
+View(mois_accident)
 
 #lm([target] ~ [predictor / features], data = [data source])
-regression <- lm(Freq ~ as.numeric(Var1), data = mois_accident)
+regression <- lm(CumFreq ~ as.numeric(Var1), data = mois_accident)
 coefficients <- summary(regression)$coefficients
 
 print(coefficients)
@@ -30,12 +30,12 @@ print(coefficients)
 mois_seq <- seq(min(as.numeric(mois_accident$Var1)), max(as.numeric(mois_accident$Var1)), length.out = 100)
 predictions <- predict(regression, newdata = data.frame(Var1 = mois_seq))
 
-ggplot(mois_accident, aes(x = Var1, y = Freq)) +
+ggplot(mois_accident, aes(x = Var1, y = CumFreq)) +
   geom_point() +
-  geom_line(data = data.frame(Var1 = mois_seq, Freq = predictions), aes(x = Var1, y = Freq), color = "red") +
+  geom_line(data = data.frame(Var1 = mois_seq, CumFreq = predictions), aes(x = Var1, y = CumFreq), color = "red") +
   labs(title = "Régression linéaire des accidents", x = "Mois", y = "Accidents")
 
-confidence_intervals <- confint(regression)
+confidence_intervals <- confint(regression, level=0.95)
 
 
 regression_summary <- summary(regression)
@@ -49,11 +49,19 @@ print(r_squared)
 print(r_squared_adjusted)
 
 mois_accident$Var1 <- as.numeric(as.character(mois_accident$Var1))
-mois_accident$Freq <- as.numeric(as.character(mois_accident$Freq))
+mois_accident$CumFreq <- as.numeric(as.character(mois_accident$CumFreq))
 
-correlation <- cor(mois_accident$Var1, mois_accident$Freq)
+correlation <- cor(mois_accident$Var1, mois_accident$CumFreq)
 
 regression_summary
 print(confidence_intervals)
 print(correlation)
+
+
+residuals <- resid(regression)
+residuals
+
+plot(mois_accident$Var1, residuals, ylab="Residus", xlab="Mois", main="Residus selon les mois")
+shapiro.test(residuals)
+durbinWatsonTest(regression)
 
--- regression_lineaire_semaine.R
+++ regression_lineaire_semaine.R
@@ -4,7 +4,6 @@ library(ggplot2)
 data <- read.csv("cleaned_with_region_population.csv", sep=",", fileEncoding="UTF-8")
 
 date <- data$date
-
 mois <- format(as.Date(date, "%m/%d/%Y %H:%M"), "%m/%d")
 
 mois <- data.frame(mois)
@@ -20,9 +19,11 @@ week_tmp <-as.data.frame(week_tmp)
 #Regression pour les semaines
 
 week_accident <- data.frame(Var1 = factor(week_tmp$Var1), Freq = week_tmp$Freq)
+week_accident$CumFreq <- cumsum(week_accident$Freq)
+
 
 #lm([target] ~ [predictor / features], data = [data source])
-regression <- lm(Freq ~ as.numeric(Var1), data = week_accident)
+regression <- lm(CumFreq ~ as.numeric(Var1), data = week_accident)
 coefficients <- summary(regression)$coefficients
 
 print(coefficients)
@@ -30,21 +31,25 @@ print(coefficients)
 week_seq <- seq(min(as.numeric(week_accident$Var1)), max(as.numeric(week_accident$Var1)), length.out = 100)
 predictions <- predict(regression, newdata = data.frame(Var1 = week_seq))
 
-ggplot(week_accident, aes(x = Var1, y = Freq)) +
+ggplot(week_accident, aes(x = Var1, y = CumFreq)) +
   geom_point() +
-  geom_line(data = data.frame(Var1 = week_seq, Freq = predictions), aes(x = Var1, y = Freq), color = "red") +
+  geom_line(data = data.frame(Var1 = week_seq, CumFreq = predictions), aes(x = Var1, y = CumFreq), color = "red") +
   labs(title = "Régression linéaire des accidents", x = "Semaines", y = "Accidents")
 
 summary(regression)
 
-confidence_intervals <- confint(regression)
-print(confidence_intervals)
-
-
-
 regression_summary <- summary(regression)
 
 #View(regression_summary) pour comprendre les affectations de données
+
+confidence_intervals <- confint(regression, level=0.95)
+print(confidence_intervals)
+
+r_squared <- regression_summary$r.squared
+r_squared_adjusted <- regression_summary$adj.r.squared
+
+print(r_squared)
+print(r_squared_adjusted)
 
 r_squared <- regression_summary$r.squared
 r_squared_adjusted <- regression_summary$adj.r.squared
@@ -53,9 +58,17 @@ print(r_squared)
 print(r_squared_adjusted)
 
 week_accident$Var1 <- as.numeric(as.character(week_accident$Var1))
-week_accident$Freq <- as.numeric(as.character(week_accident$Freq))
+week_accident$CumFreq <- as.numeric(as.character(week_accident$CumFreq))
 
-correlation <- cor(week_accident$Var1, week_accident$Freq)
+correlation <- cor(week_accident$Var1, week_accident$CumFreq)
 print(correlation)
 
+confidence_intervals <- confint(regression, level=0.95)
+print(confidence_intervals)
 
+residuals <- resid(regression)
+residuals
+
+plot(week_accident$Var1, residuals, ylab="Residus", xlab="Semaines", main="Residus selon les semaines")
+shapiro.test(residuals)
+durbinWatsonTest(regression)
